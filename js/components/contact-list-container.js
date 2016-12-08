cb89c63992aa4b736e3443a37d9cefc511fb2c5e
davellaj <jamie.davella@gmail.com>
2016-12-07 23:45:51 -0500

    change import from react as well as import ContactList'

--- js/components/contact-list-container.js
+++ js/components/contact-list-container.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import CONTACTS from '../contacts';
 
+import ContactList from './contact-list'
+
 export default function ContactListContainer() {
 	return <ContactList contacts={CONTACTS} />;
-};
+}; 
