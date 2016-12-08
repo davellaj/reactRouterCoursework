bd1c01598312044b921b9c60dc0df563a4db9723
davellaj <jamie.davella@gmail.com>
2016-12-08 01:06:05 -0500

    create app component to now hold props.children and make app the parent route with ContactListContainer as the nested route

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ require('babel-polyfill');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 
 import ContactListContainer from './components/contact-list-container';
 import App from './components/app';
